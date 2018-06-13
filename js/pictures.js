cabc1f9c40a90bbb362bb890b3e5793c2d6587fb
Anya Vigolajnen <avi26.job@yandex.ru>
2018-06-13 23:07:36 +0300

    задание - пока все дома

--- js/pictures.js
+++ js/pictures.js
@@ -6,61 +6,93 @@ var LIKES_MAX = 200;
 var photosComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var photosDescriptions = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. ', 'Не обижайте всех словами......', 'Вот это тачка!'];
 
-// случайное значение в интервале
-var getRandomNumber = function (min, max) {
-  var rand = Math.floor(min + Math.random() * (max + 1 - min));
-  return rand;
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// случайное значение из массива
 var getRandomValue = function (array) {
-  return array[Math.floor(Math.random() * array.length)];
+  var rand = getRandomInt(0, array.length - 1);
+  return array[rand];
 };
 
 var createPhotos = function () {
   return {
-    url: 'photos/' + getRandomNumber(1, QUANTITY_IMAGES) + '.jpg',
-    likes: getRandomNumber(LIKES_MIN, LIKES_MAX),
+    url: 'photos/' + getRandomInt(1, QUANTITY_IMAGES) + '.jpg',
+    likes: getRandomInt(LIKES_MIN, LIKES_MAX),
     comments: getRandomValue(photosComments),
     description: getRandomValue(photosDescriptions)
   };
 };
 
-// 2. На основе данных, созданных в предыдущем пункте и шаблона #picture создайте DOM-элементы
-var renderPhotos = function (photo) {
-  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
-  var pictureElement = pictureTemplate.cloneNode(true);
-
-  pictureElement.querySelector('.picture__img').src = photo.url;
-  pictureElement.querySelector('.picture__stat--likes').textContent = photo.likes;
-  pictureElement.querySelector('.picture__stat--comments').textContent = photo.comments;
-
-  return pictureElement;
-};
-
-// 3. Отрисуйте сгенерированные DOM-элементы в блок .pictures
-var pictureListElement = document.querySelector('.pictures');
-var fragment = document.createDocumentFragment();
-for (var i = 1; i <= QUANTITY_IMAGES; i++) {
-  fragment.appendChild(renderPhotos(createPhotos()));
+var photos = [];
+for (var j = 0; j < QUANTITY_IMAGES; j++) {
+  photos.push(createPhotos());
 }
-pictureListElement.appendChild(fragment);
+
+// 2. На основе данных, созданных в предыдущем пункте и шаблона #picture создайте DOM-элементы
+// 3. Отрисуйте сгенерированные DOM-элементы в блок .pictures
+
+var renderPhotos = function () {
+  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
+  var pictureListElement = document.querySelector('.pictures');
+  var documentFragment = document.createDocumentFragment();
+  for (var i = 0; i < photos.length; i++) {
+    var pictureElement = pictureTemplate.cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').src = photos[i].url;
+    pictureElement.querySelector('.picture__stat--likes').textContent = photos[i].likes;
+    pictureElement.querySelector('.picture__stat--comments').textContent = photos[i]['comments'].length;
+
+    documentFragment.appendChild(pictureElement);
+  }
+  return pictureListElement.appendChild(documentFragment);
+};
 
 // 4. Покажите элемент .big-picture и заполните его данными из первого элемента сгенерированного вами массива
 document.querySelector('.big-picture').classList.remove('hidden');
 
-var renderBigPhotos = function (photo) {
+var renderBigPhotos = function () {
   var pictureTemplate = document.querySelector('.big-picture__preview');
+  var pictureBigElement = document.querySelector('.big-picture');
+  var fragmentModal = document.createDocumentFragment();
   var pictureElement = pictureTemplate.cloneNode(true);
 
-  pictureElement.querySelector('.big-picture__img img').src = photo.url;
-  pictureElement.querySelector('.likes-count').textContent = photo.likes;
-  pictureElement.querySelector('.comments-count').textContent = photo['comments'].length;
+  pictureElement.querySelector('.big-picture__img img').src = photos[0].url;
+  pictureElement.querySelector('.likes-count').textContent = photos[0].likes;
+  pictureElement.querySelector('.comments-count').textContent = photos[0]['comments'].length;
+  pictureElement.querySelector('.social__caption').textContent = photos[0].description;
 
-  return pictureElement;
+  fragmentModal.appendChild(pictureElement);
+
+  return pictureBigElement.appendChild(fragmentModal);
 };
 
-var pictureBigElement = document.querySelector('.big-picture');
-var fragmentModal = document.createDocumentFragment();
-fragmentModal.appendChild(renderBigPhotos(createPhotos(1)));
-pictureBigElement.appendChild(fragmentModal);
+var commentsBigPhotos = function () {
+  var commentTemplate = document.querySelector('.social__comment');
+  var commentListElement = document.querySelector('.social__comments');
+  var commentFragment = document.createDocumentFragment();
+  var commentElement = commentTemplate.cloneNode(true);
+  var countComment = photos[0].comments.length;
+
+  for (var i = 0; i < countComment; i++) {
+    commentElement.querySelector('.social__picture').src = 'img/avatar-' + getRandomInt(1, 6) + '.svg';
+    commentElement.querySelector('.social__text').textContent = photos[0].comments[i];
+
+    commentFragment.appendChild(commentElement);
+  }
+  return commentListElement.appendChild(commentFragment);
+};
+// console.log(commentsBigPhotos());
+// 5. Спрячьте блоки счётчика комментариев .social__comment-count и загрузки новых комментариев .social__loadmore, добавив им класс .visually-hidden.
+var hiddenElement = function () {
+  var list = document.querySelectorAll('.social__comment-count, .social__loadmore');
+  for (var i = 0; i < list.length; i++) {
+    list[i].classList.add('visually-hidden');
+  }
+  return;
+};
+
+renderPhotos();
+renderBigPhotos();
+commentsBigPhotos();
+hiddenElement();
