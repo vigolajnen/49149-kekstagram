d3b07232b71f644edeb6b798bc1645be351c0784
Anna Vigolajnen <vigolajnen@gmail.com>
2018-07-19 15:01:33 +0300

    добавила комментарии

--- js/pictures.js
+++ js/pictures.js
@@ -7,6 +7,7 @@ var LIKES_MAX = 200;
 var photosComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var photosDescriptions = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. ', 'Не обижайте всех словами......', 'Вот это тачка!'];
 
+// вспомогательные функции
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -76,7 +77,6 @@ var renderBigPhotos = function (photo) {
 };
 
 var commentsBigPhotos = function () {
-
   var commentTemplate = document.querySelector('.social__comment');
   var commentListElement = document.querySelector('.social__comments');
   var commentFragment = document.createDocumentFragment();
@@ -201,7 +201,7 @@ var getEffect = function () {
   });
 };
 
-// передвижение пина - mousedown, mousemove, mouseup
+// передвижение маркера - mousedown, mousemove, mouseup
 scrollBar.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
   var widthScrollBar = lineScrollBar.clientWidth;
@@ -242,7 +242,7 @@ var changeEffectIntensity = function (value) {
   levelScrollBar.style.width = value + '%';
 };
 
-// получение имени эффекта и значение фильтра
+// получение названия эффекта и значение фильтра
 var getEffectStyle = function (name, value) {
   switch (name) {
     case 'chrome':
